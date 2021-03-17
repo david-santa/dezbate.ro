e42ad90a55d99436986f12dc49eedc8c72582a12
David Santa <santadavid18@stud.ase.ro>
2021-03-17 21:08:15 +0200

    Sign Up and Sign In with Email/Pass Working

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,30 +6,50 @@ import logo from "../res/logoDezbateRo.png"
 import Button from "@material-ui/core/Button";
 import firebase from 'firebase';
 import {signInWithGoogle} from "../firebase/firebaseUtils";
-import {AccountCircle, LockRounded} from "@material-ui/icons"
+import {AccountCircle, Email, LockRounded} from "@material-ui/icons"
 import {CgFacebook, CgGoogle} from "react-icons/all";
 
-export default class Login extends React.Component{
+export default class Login extends React.Component {
     constructor(props) {
         super(props);
     }
 
-    state={
-        isLogin:true,
-        emailInput:'',
-        passInput:''
+    state = {
+        isLogin: true,
+        emailInput: '',
+        userInput: '',
+        passInput: '',
+        confirmPassInput: ''
     }
 
-    handleSignupButton = () => {
+
+    handleInputChange = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    handleSwitchButton = () => {
         this.setState({isLogin: !this.state.isLogin});
     }
 
-    //TODO: Delete this when you have proper sign up
-    handleTestSignup = () =>{
-        let email = "davidsanta1999@gmail.com";
-        let password = "tecsoresmecher99";
-        firebase.auth().createUserWithEmailAndPassword(email, password)
+    handleEmailPassSignIn = () => {
+        console.log(this.state.userInput + " " + this.state.passInput)
+        firebase.auth().signInWithEmailAndPassword(this.state.userInput, this.state.passInput)
             .then((userCredential) => {
+                // Signed in
+                var user = userCredential.user;
+                console.log(user)
+                // ...
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+            });
+    }
+
+    handleSignUp = () => {
+        firebase.auth().createUserWithEmailAndPassword(this.state.emailInput, this.state.passInput)
+            .then((userCredential) => {
+                console.log("Sign up pressed!")
                 // Signed in
                 var user = userCredential.user;
                 // ...
@@ -39,67 +59,74 @@ export default class Login extends React.Component{
                 var errorCode = error.code;
                 var errorMessage = error.message;
                 // ..
-            });
-    }
-
-    handleEmailPassSignIn = () =>{
-        let email = "davidsanta1999@gmail.com";
-        let password = "tecsoresmecher99";
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then((userCredential) => {
-                // Signed in
-                var user = userCredential.user;
-                // ...
-            })
-            .catch((error) => {
-                var errorCode = error.code;
-                var errorMessage = error.message;
+                console.log(errorCode)
+                console.log(errorMessage)
             });
     }
 
     render() {
-        return(
-        <Grid container>
-            <Grid item xs={false} sm={4} md={7} style={{height:'100vh'}}>
-                <img src = {loginImage} style = {{width: '100%',height:'100%', objectFit: 'stretch'}}/>
-            </Grid>
-            <Grid container item xs={12} sm={8} md={5} alignItems={"center"} direction={"column"} justify={"space-between"} style ={{background:"#212121",color:"aliceblue",}}>
-                <div/>
-                <div style = {{display:'flex', flexDirection:'column', maxWidth:400, color:"white"}}>
-                    <Grid container justify="center">
-                        <img src={logo} width = "150"/>
-                    </Grid>
-                    <FormControl>
-                        <InputLabel htmlFor={"userInput"} style = {{color:'aliceblue'}}> Username </InputLabel>
-                        <Input startAdornment={<AccountCircle/>} id = "userInput" style={{color:"aliceblue"}}/>
-                    </FormControl>
-                    <br/>
-                    <FormControl>
-                        <InputLabel htmlFor={"passwordInput"} style = {{color:'aliceblue'}}> Password </InputLabel>
-                        <Input startAdornment={<LockRounded/>} id = "passwordInput" type={"password"} style={{color:"aliceblue"}}/>
-                    </FormControl>
-                    <br/>
-                    {this.state.isLogin?
-                        <></>
-                        :
-                        <FormControl>
-                        <InputLabel htmlFor={"passwordInput"} style = {{color:'aliceblue'}}> Confirm password </InputLabel>
-                        <Input startAdornment={<LockRounded/>} id = "confirmPasswordInput" type={"password"} style={{color:"aliceblue"}}/>
-                        </FormControl>
+        return (
+            <Grid container>
+                <Grid item xs={false} sm={4} md={7} style={{height: '100vh'}}>
+                    <img src={loginImage} style={{width: '100%', height: '100%', objectFit: 'stretch'}}/>
+                </Grid>
+                <Grid container item xs={12} sm={8} md={5} alignItems={"center"} direction={"column"}
+                      justify={"space-between"} style={{background: "#212121", color: "aliceblue",}}>
+                    <div/>
+                    <div style={{display: 'flex', flexDirection: 'column', maxWidth: 400, color: "white"}}>
+                        <Grid container justify="center">
+                            <img src={logo} width="150"/>
+                        </Grid>
+                        {this.state.isLogin ?
+                            <></>
+                            :
+                            <FormControl>
+                                <InputLabel htmlFor={"emailInput"} style={{color: 'aliceblue'}}> Email </InputLabel>
+                                <Input name={"emailInput"} value={this.state.emailInput}
+                                       onChange={this.handleInputChange} startAdornment={<Email/>} id="emailInput"
+                                       style={{color: "aliceblue"}}/>
+                            </FormControl>
                         }
-                    <div style={{height: 20}}/>
-                    <Button onClick={this.handleEmailPassSignIn} color={"secondary"} variant ={"contained"}>Log In </Button>
-                    <h5 align={'center'}>OR</h5>
-                    <Button onClick = {signInWithGoogle} style={{background:'white'}} variant ={"contained"}> <CgGoogle/> {'\t Sign in with Google'} </Button>
-                    <Button color = "primary" onClick = {signInWithGoogle} variant ={"contained"}> <CgFacebook/> {'\t Sign in with Facebook'} </Button>
-                    <Button onClick={this.handleSignupButton} style={{color: 'aliceblue'}}> {this.state.isLogin?"Don't have an account? Sign Up":"Already have an account? Log In"} </Button>
-
-                    //TODO: Delete this when you have proper sign up
-                    <Button onClick={this.handleTestSignup} > Test Sign-Up - STERGE DUPA CE TESTEZI BOULE </Button>
-                </div>
-                <div/>
+                        <br/>
+                        <FormControl>
+                            <InputLabel htmlFor={"userInput"} style={{color: 'aliceblue'}}> Username </InputLabel>
+                            <Input name={"userInput"} value={this.state.userInput} onChange={this.handleInputChange}
+                                   startAdornment={<AccountCircle/>} id="userInput" style={{color: "aliceblue"}}/>
+                        </FormControl>
+                        <br/>
+                        <FormControl>
+                            <InputLabel htmlFor={"passwordInput"} style={{color: 'aliceblue'}}> Password </InputLabel>
+                            <Input value={this.state.passInput} name={"passInput"} onChange={this.handleInputChange}
+                                   startAdornment={<LockRounded/>} id="passwordInput" type={"password"}
+                                   style={{color: "aliceblue"}}/>
+                        </FormControl>
+                        <br/>
+                        {this.state.isLogin ?
+                            <></>
+                            :
+                            <FormControl>
+                                <InputLabel htmlFor={"confirmPassInput"} style={{color: 'aliceblue'}}> Confirm
+                                    password </InputLabel>
+                                <Input name={"confirmPassInput"} onChange={this.handleInputChange}
+                                       startAdornment={<LockRounded/>} id="confirmPassInput" type={"password"}
+                                       style={{color: "aliceblue"}}/>
+                            </FormControl>
+                        }
+                        <div style={{height: 20}}/>
+                        <Button onClick={this.state.isLogin ? this.handleEmailPassSignIn : this.handleSignUp}
+                                color={"secondary"}
+                                variant={"contained"}>{this.state.isLogin ? "Log In" : "Sign up"}</Button>
+                        <h5 align={'center'}>OR</h5>
+                        <Button onClick={signInWithGoogle} style={{background: 'white'}} variant={"contained"}>
+                            <CgGoogle/> {'\t Sign in with Google'} </Button>
+                        <Button color="primary" onClick={signInWithGoogle} variant={"contained"}>
+                            <CgFacebook/> {'\t Sign in with Facebook'} </Button>
+                        <Button onClick={this.handleSwitchButton}
+                                style={{color: 'aliceblue'}}> {this.state.isLogin ? "Don't have an account? Sign Up" : "Already have an account? Log In"} </Button>
+                    </div>
+                    <div/>
+                </Grid>
             </Grid>
-        </Grid>
         );
     }
 }
