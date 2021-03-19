ec0a2ebc6c16e2c262b48a571090a49465e7f31d
David Santa <santadavid18@stud.ase.ro>
2021-03-19 18:53:01 +0200

    Added Profile Menu Functionality

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,19 @@ import {Redirect} from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import logoFaraText from '../res/logoFaraText.png'
 import {withStyles} from '@material-ui/core/styles'
-import {AppBar, Toolbar} from "@material-ui/core";
+import {
+    Accordion,
+    AccordionSummary,
+    AppBar,
+    Card,
+    CardContent,
+    Menu, MenuItem,
+    Paper,
+    Toolbar,
+    Typography
+} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import {ExpandMore} from "@material-ui/icons";
 
 const styles = (theme) => ({
     snapRight: {
@@ -15,6 +27,18 @@ const styles = (theme) => ({
 class Dashboard extends React.Component {
     constructor(props) {
         super(props)
+    }
+
+    state = {
+        menuAnchor: null
+    }
+
+    handleExpandMenu = (e) => {
+        this.setState({menuAnchor: e.currentTarget});
+    }
+
+    handleCloseMenu = () => {
+        this.setState({menuAnchor: null})
     }
 
     render() {
@@ -29,19 +53,64 @@ class Dashboard extends React.Component {
                                 style={{padding: '10px 10px 10px 20px', 'fontSize': '18px'}}>Descopera</Button>
                         <Button size={"large"} color={"secondary"} style={{padding: '10px', 'fontSize': '18px'}}>Incepe
                             o dezbatere</Button>
-                        <div style={{display:'flex', flexDirection:"row", alignItems:'center'}} className={classes.snapRight}>
+                        <div style={{display: 'flex', flexDirection: "row", alignItems: 'center'}}
+                             className={classes.snapRight}>
                             <p className={classes.snapRight}>{this.props.currentUser ? this.props.currentUser.displayName : <></>}</p>
-                            <img height={"35px"} style={{"borderRadius": "50%", "marginLeft": "10px", position:"relative"}}
+                            <img height={"35px"}
+                                 style={{"borderRadius": "50%", "marginLeft": "10px", position: "relative"}}
                                  src={this.props.currentUser ? this.props.currentUser.photoURL : ""}/>
+                            <Button
+                                onClick={this.handleExpandMenu}> <ExpandMore/> </Button>
+                            <Menu
+                                id={'profileMenu'}
+                                anchorEl={this.state.menuAnchor}
+                                keepMounted
+                                open={Boolean(this.state.menuAnchor)}
+                                onClose={this.handleCloseMenu}
+                            >
+                                <MenuItem onClick={this.handleCloseMenu}>Profile</MenuItem>
+                                <MenuItem onClick={this.handleCloseMenu}>My account</MenuItem>
+                                <MenuItem onClick={this.handleCloseMenu}>Logout</MenuItem>
+                            </Menu>
+
                         </div>
                     </Toolbar>
                 </AppBar>
+
                 <div>
                     {console.log(this.currentUser)}
                     {this.props.currentUser ?
-                        <div><h1>I am the Dashboard!</h1> <Button onClick={() => auth.signOut()}> Sign Out </Button>
+                        <div><h1>I am the Dashboard!</h1>
                         </div> : <Redirect to="/"/>}
                 </div>
+
+                <div>
+                    <Paper>
+                        <Grid container>
+                            <Grid item>
+                                <Card>
+                                    <CardContent>
+                                        <Typography>
+                                            hau
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+
+                            <Grid item>
+                                <Card>
+                                    <CardContent>
+                                        <Typography>
+                                            hau2
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </div>
+
+
             </div>
         )
     }
