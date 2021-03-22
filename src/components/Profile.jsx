b558a1cca4abf6a63913c4165b87ba8b61c68d3d
David Santa <santadavid18@stud.ase.ro>
2021-03-22 16:28:04 +0200

    Added Profile.jsx

---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+export default class Profile extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <>
+                <h1>profile</h1>
+            </>
+        )
+    }
+}
