1e9e5c39a85be9f37a92c47785ec161b98da8ad6
David Santa <santadavid18@stud.ase.ro>
2021-03-23 21:17:03 +0200

    Just a daily commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-export default class Profile extends React.Component{
+export default class Profile extends React.Component {
     constructor(props) {
         super(props);
         console.log(props)
     }
 
     render() {
-        return(
+        return (
             <>
                 <h1>profile</h1>
             </>
