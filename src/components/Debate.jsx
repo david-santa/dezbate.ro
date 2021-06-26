56422d4b9021c0c9f84630d2efef4ee283161fde
David <santadavid18@stud.ase.ro>
2021-06-26 11:48:37 +0300

    better design for debate page

--- src/components/Debate.jsx
+++ src/components/Debate.jsx
@@ -75,46 +75,54 @@ class Debate extends React.Component {
                     <Topbar/>
                 </div>
                 <br/> <br/> <br/>
-                <div style={{padding: '5vh 10vw 0vh 10vw'}}>
-                    <Card id={"title"} style={{background: '#3f51b5'}}>
-                        <CardContent>
+                <div style={{margin: '5vh 20vw 0vh 20vw'}}>
+                    <Grid container justify={"center"} direction={"column"} alignItems={"center"} spacing={1}
+                          style={{background: '#3f51b5'}}>
+                        <Grid item style={{background: '#3f51b5'}}>
                             <Typography variant={"h4"} gutterBottom
                                         style={{textAlign: "center"}}>
                                 {this.state.debate.title}
                             </Typography>
-                        </CardContent>
-                    </Card>
-                    <Grid container justify={"center"} direction={"row"} alignItems={"center"} spacing={1}>
-                        <Grid container justify={"flex-start"} direction={"column"} alignItems={"flex-end"} spacing={2}
-                              style={{maxWidth: '30vw', marginRight: '1vw'}}>
-                            <Button variant='outlined' style={{background: '#41cc90'}}>
-                                Adauga argument pro
-                            </Button>
-                            {this.state.proArgumentsArray.map((value, index) => (
-                                <Grid key={index} direction={"column"} item>
-                                    <Card style={{maxWidth: '15vw', background: '#1f4068'}}>
-                                        <CardContent>
-                                            {value.content}
-                                        </CardContent>
-                                    </Card>
-                                </Grid>
-                            ))}
                         </Grid>
-                        <Divider orientation={'vertical'} flexItem/>
-                        <Grid container justify={"flex-start"} direction={"column"} alignItems={"flex-start"}
-                              spacing={2} style={{maxWidth: '30vw', marginLeft: '1vw', paddingTop: '1vh'}}>
-                            <Button variant='outlined' style={{background: '#ff725c'}}>
-                                Adauga argument contra
-                            </Button>
-                            {this.state.conArgumentsArray.map((value, index) => (
-                                <Grid key={index} item>
-                                    <Card style={{maxWidth: '15vw'}}>
-                                        <CardContent>
-                                            {value.content}
-                                        </CardContent>
-                                    </Card>
-                                </Grid>
-                            ))}
+                        <Grid container direction={'row'} alignItems={'flex-start'} justify={"center"}>
+                            <Grid container justify={"flex-start"} direction={"column"} alignItems={"stretch"}
+                                  spacing={2}
+                                  style={{
+                                      maxWidth: '20vw',
+                                      marginRight: '1vw',
+                                      marginBottom: '1vh',
+                                      paddingTop: '1vh'
+                                  }}>
+                                <Button variant='outlined' style={{background: '#41cc90', width:'100%'}}>
+                                    Adauga argument pro
+                                </Button>
+                                {this.state.proArgumentsArray.map((value, index) => (
+                                    <Grid key={index} direction={"column"} item>
+                                        <Card style={{maxWidth: '100%', background: '#1f4068'}}>
+                                            <CardContent>
+                                                {value.content}
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+                                ))}
+                            </Grid>
+                            <Divider orientation={'vertical'} flexItem/>
+                            <Grid container justify={"flex-start"} direction={"column"} alignItems={""}
+                                  spacing={2}
+                                  style={{maxWidth: '20vw', marginLeft: '1vw', marginBottom: '1vh', paddingTop: '1vh'}}>
+                                <Button variant='outlined' style={{background: '#ff725c'}}>
+                                    Adauga argument contra
+                                </Button>
+                                {this.state.conArgumentsArray.map((value, index) => (
+                                    <Grid key={index} item>
+                                        <Card style={{width: '100%',background: '#1f4068'}}>
+                                            <CardContent>
+                                                {value.content}
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+                                ))}
+                            </Grid>
                         </Grid>
                     </Grid>
                 </div>
@@ -123,8 +131,14 @@ class Debate extends React.Component {
     }
 }
 
-const mapStateToProps = ({user}) => ({
-    currentUser: user.currentUser
-})
+const mapStateToProps = (
+    {
+        user
+    }
+) => (
+    {
+        currentUser: user.currentUser
+    }
+)
 
 export default connect(mapStateToProps)(withStyles(styles)(Debate))
