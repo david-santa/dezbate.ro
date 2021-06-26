2963c102a32d1ae72208f5c5e22975692d2c0a42
David <santadavid18@stud.ase.ro>
2021-06-26 17:31:21 +0300

    Added Menu implementation to DebateCard on Dashboard

--- src/components/DebateCard.jsx
+++ src/components/DebateCard.jsx
@@ -1,4 +1,14 @@
-import {Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography} from "@material-ui/core";
+import {
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    IconButton,
+    Menu,
+    MenuItem,
+    Typography
+} from "@material-ui/core";
 import {Chat, GroupSharp, MoreVert, Visibility} from "@material-ui/icons";
 import React from "react";
 import {Link} from "react-router-dom";
@@ -8,32 +18,57 @@ export default class DebateCard extends React.Component {
         super(props);
     }
 
+    state = {
+        anchorEl: null
+    }
+
+    handleOptions = (event) => {
+        this.setState({anchorEl: event.currentTarget})
+    }
+
+    handleClose = () => {
+        this.setState({anchorEl: null})
+    }
+
+    handleReport = () => {
+        console.log("report")
+    }
+
     render() {
         return (
             <div>
                 <Card variant={'outlined'}
                       style={{width: 'max(350px,35vh)', background: "#1f4068", color: '#ececec'}}>
                     <CardHeader
-                        action={<IconButton aria-label='settings'>
+                        action={<IconButton onClick={this.handleOptions} aria-label='settings'>
                             <MoreVert/>
                         </IconButton>}
                         title={<Typography align={'center'} autoCapitalize={'true'}>
 
-                                {this.props.titlu}
+                            {this.props.titlu}
                         </Typography>}
                     />
+                    <Menu
+                        id={'optionsMenu'}
+                        anchorEl={this.state.anchorEl}
+                        keepMounted
+                        open={Boolean(this.state.anchorEl)}
+                        onClose={this.handleClose}
+                    >
+                        <MenuItem onClick={this.handleReport}> Raporteaza o problema </MenuItem>
+                    </Menu>
                     <CardMedia>
                         <Link to={'/debate?uid=' + this.props.id} style={{textDecoration: 'none'}}>
-                        <img
-                            style={{
-                                'display': 'block',
-                                marginLeft: 'auto',
-                                marginRight: 'auto',
-                                width: '80%'
-                            }}
-                            src={this.props.imagine}
-                            alt={"There should be an image here but it's currently unavailable"}
-                        />
+                            <img
+                                style={{
+                                    'display': 'block',
+                                    marginLeft: 'auto',
+                                    marginRight: 'auto',
+                                    width: '80%'
+                                }}
+                                src={this.props.imagine}
+                                alt={"There should be an image here but it's currently unavailable"}
+                            />
                         </Link>
                     </CardMedia>
                     <CardContent>
